9b2414d911f1e5b3bc4bf153f90f5549d36bac57
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2023-03-22 23:18:48 +0800

    Added more security for Auth

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   # belongs_to :user
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :user_ID, presence: true
+  
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,7 +2,7 @@ class Repository
   def self.create_user_account(new_user)
     @user_details = User.new("first_name": new_user['first_name'], "last_name": new_user['last_name'], "user_mail": new_user['email'])
     # @user_details.save
-    return false if !(@user_details.save)
+    return user_details.errors.full_messages if !(@user_details.save)
     
     @account_details = Account.new( "user_ID":  User.last.id, "account_type_ID": 1, "username": new_user['username'], "password": new_user['password'], "authentication_token": "randomCode")
 
@@ -20,7 +20,14 @@ class Repository
 
   def self.validate_user_account(validate_user)    
     @account = Account.find_by("username": validate_user["username_email"])
+    if @account
+      @saved_pw = @account.password
+    end
     @user = User.find_by("user_mail": validate_user["username_email"])
+    if @user
+      @account = Account.find_by("user_ID": @user.id)
+      # @saved_pw = @user_obj.password
+    end
     @generated_token = (0...50).map { ('a'..'z').to_a[rand(26)] }.join 
      #https://stackoverflow.com/questions/88311/how-to-generate-a-random-string-in-ruby
 
@@ -39,8 +46,7 @@ class Repository
   end
 
   def self.allow_access(user_token)
-    
-
+    @logged_account = Account.find_by("authentication_token": user_token)
   end
 
  

cea5c88d371c1359268b33ed52426d378fb4b50d
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2023-03-23 23:16:57 +0800

    Create Categories Complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   get 'pages/login'
   get 'pages/register'
 
+  get 'pages/categories/all' => 'categories#my_categories'
+
 
   post '/authentications/signUp(.:format)' => 'authentications#new_user'
   post '/authentications/signIn(.:format)' => 'authentications#verify_user'
   post '/authentications/signOut' => 'authentications#clear_user'
 
-  post 'pages/categories/create(.:format)' => 'categories#create'
+  post '/pages/categories/create(.:format)' => 'categories#create'
+  # post 'pages/categories/create' => 'categories#create'
    
   get '/' => "pages#index"
   get 'pages/dashboard' => "pages#dashboard"

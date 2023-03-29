8d4c260fa5ce60db0fbe5b2bd3d0479f96e49b5d
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2023-03-29 22:56:40 +0800

    Added- bcrypt

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ApplicationRecord
   # belongs_to :user
+  # has_secure_password
   validates :username, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :user_ID, presence: true
